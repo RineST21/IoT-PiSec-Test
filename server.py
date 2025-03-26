fcea02b020a9a32eab395462ce82eace13c405d5
Marcin Lewandowski <86814916+RineST21@users.noreply.github.com>
2025-03-26 15:57:35 +0100

    Update server.py

--- server.py
+++ server.py
@@ -1,10 +1,9 @@
 from flask import Flask, request
 import mysql.connector
-from datetime import datetime
 
 app = Flask(__name__)
 
-#Remember to change it!
+# Remember to change it!
 db_config = {
     'user': 'root',      
     'password': 'raspberry',      
@@ -17,21 +16,28 @@ db_config = {
 def receive_data():
     temperature = request.args.get('temperature')
     humidity = request.args.get('humidity')
+    pressure = request.args.get('pressure')
+    timestamp = request.args.get('timestamp')
     
-    if temperature and humidity:
+    if temperature and humidity and pressure:
         try:
             humidity = float(humidity)
             temperature = float(temperature)
-            timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-
+            pressure = float(pressure)
+            
             conn = mysql.connector.connect(**db_config)
             c = conn.cursor()
+            
             c.execute("INSERT INTO DHT11_measurement (Date, Temperature, Humidity) VALUES (%s, %s, %s)",
                       (timestamp, temperature, humidity))
+            
+            c.execute("INSERT INTO BMP280_measurement (Date, Pressure) VALUES (%s, %s)",
+                      (timestamp, pressure))
+            
             conn.commit()
             conn.close()
 
-            print(f"Received: timestamp={timestamp}, temperature={temperature}, humidity={humidity}")
+            print(f"Received: timestamp={timestamp}, temperature={temperature}, humidity={humidity}, pressure={pressure}")
             return "Data saved successfully", 200
         except Exception as e:
             print(f"Error: {e}")
